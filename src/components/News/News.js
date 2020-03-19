afd5b8979ca7e15d7afa3584edb1093ed4668b85
ernesthenry <henry38ernest@gmail.com>
2020-03-20 01:15:50 +0300

    fetch data and update state

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -2,18 +2,41 @@ import React, { Component } from 'react'
 import NewSingle from './NewSingle'
 
 class News extends Component {
+    constructor(props){
+        super(props);
+        this.state ={
+            news: []
+        }
+    }
+
+    componentDidMount(){
+        const url ="http://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=6b21cf68205242f1a125cd091578c4f8";
+
+        fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            this.setState({
+                news: data.articles
+            })
+        })
+        .catch((error) => console.log(error));
+
+    }
     renderItems(){
-        return this.props.items.map((item) => (
-            <NewSingle  key={item.id} item={item}/>
+        return this.state.news.map((item) => (
+            <NewSingle  key={item.url} item={item}/>
         ));
     }
     render() {
         return (
-            <div>
-                <ul>{this.renderItems()}</ul>
+            <div className="row">
+                {this.renderItems()}
             </div>
         )
     }
 }
 
 export default News
+
