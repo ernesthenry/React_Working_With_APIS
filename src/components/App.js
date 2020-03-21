af8d32b6e558e4d6f2f6feaecdd5fa29522ac375
ernesthenry <henry38ernest@gmail.com>
2020-03-21 10:15:36 +0300

    Refactor componemt for reuse

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,34 @@ import React from 'react';
 import './App.css';
 import News from './News/News'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-         <p>My Feed</p>
-      </header>
-      <News />
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    news1: {
+      type: "top-headlines",
+      query: "sources=bbc-news"
+    }, 
+
+    news2: {
+      type: "everything",
+      query: "domains=techcrunch.com&language=en"
+    }
+  }
+}
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+           <p>My Feed</p>
+        </header>
+        <News news={this.state.news1}/>
+        <News news={this.state.news2}/>
+      </div>
+    );
+
+  }
+  
 }
 
 export default App;
