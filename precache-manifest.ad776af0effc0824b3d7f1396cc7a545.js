e1382c03ec4c57a735be7070b36b309a5cc00701
ernesthenry <henry38ernest@gmail.com>
2020-06-13 22:35:47 +0300

    Updates

---
+++ precache-manifest.ad776af0effc0824b3d7f1396cc7a545.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c72a9b9c42b03370d08d2efe7afe8ff",
+    "url": "/React_Working_With_APIS/index.html"
+  },
+  {
+    "revision": "4b5ffaba832fca113aa7",
+    "url": "/React_Working_With_APIS/static/css/main.8db714bf.chunk.css"
+  },
+  {
+    "revision": "d23fa97d2be5b8f50642",
+    "url": "/React_Working_With_APIS/static/js/2.1f0d92f7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/React_Working_With_APIS/static/js/2.1f0d92f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b5ffaba832fca113aa7",
+    "url": "/React_Working_With_APIS/static/js/main.a67b51fa.chunk.js"
+  },
+  {
+    "revision": "84447bcd95b80e757b22",
+    "url": "/React_Working_With_APIS/static/js/runtime-main.f250fe4e.js"
+  }
+]);
